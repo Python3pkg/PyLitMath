11f713a1e7fea6c3a55d8783ec307f237e312447
WangYao <wangyao105211@163.com>
2017-04-08 22:02:53 +0800

    Quaternion

--- LitMath/Quaternion.py
+++ LitMath/Quaternion.py
@@ -6,26 +6,27 @@ class Quaternion(object):
     __hash__ = None
     
     def __init__(self, x=0.0, y=0.0, z=0.0, w=1.0):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.w = w
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+        self.w = float(w)
         
     def set(self, x, y, z, w):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.w = w
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+        self.w = float(w)
         return self
         
-    def __copy__(self):
-        return Quaternion(self.x, self.y, self.z, self.w)
-    copy = __copy__
+    def copy(self):
+        import copy
+        return copy.copy(self)
     
     def __repr__(self):
-        return 'Quaternion(%.2f, %.2f, %.2f, %.2f)' % \
+        return 'Quaternion( %.2f, %.2f, %.2f, %.2f )' % \
                (self.x, self.y, self.z, self.w)
-            
+    
+    @property    
     def magnitude(self):
         return math.sqrt(self.x ** 2 +
                          self.y ** 2 +
@@ -33,16 +34,17 @@ class Quaternion(object):
                          self.w ** 2)
             
     def normalize(self):
-        len = self.magnitude()
+        len = self.magnitude
         if len != 0:
             self.x /= len
             self.y /= len
             self.z /= len
             self.w /= len   
         return self
-        
+    
+    @property
     def normalized(self):
-        len = self.magnitude()
+        len = self.magnitude
         if len == 0:
             return self.copy()
         else:
@@ -53,7 +55,8 @@ class Quaternion(object):
         self.y = -self.y
         self.z = -self.z
         return self
-        
+    
+    @property    
     def inverse(self):
         return Quaternion(-self.x, -self.y, -self.z, self.w)
                
@@ -69,7 +72,7 @@ class Quaternion(object):
     def multiplyPoint(self, pnt):
         '''Rotates the point pnt by this quaternion.'''
         import Vector3
-        assert isinstance(other, Vector3)
+        assert isinstance(pnt, Vector3.Vector3)
         
         x = self.x
         y = self.y
@@ -80,11 +83,11 @@ class Quaternion(object):
         z2 = self.z * self.z
         w2 = self.w * self.w
         
-        dx = (x2+w2-y2-z2)*other.x + 2.0*(x*y-z*w)*other.y + 2.0*(x*z+y*w)*other.z
-        dy = 2.0*(x*y+z*w)*other.x + (w2-x2+y2-z2)*other.y + 2.0*(y*z-x*w)*other.z
-        dz = 2.0*(x*z-y*w)*other.x + 2.0*(x*w+y*z)*other.y + (w2-x2-y2+z2)*other.z
+        dx = (x2+w2-y2-z2)*pnt.x + 2.0*(x*y-z*w)*pnt.y + 2.0*(x*z+y*w)*pnt.z
+        dy = 2.0*(x*y+z*w)*pnt.x + (w2-x2+y2-z2)*pnt.y + 2.0*(y*z-x*w)*pnt.z
+        dz = 2.0*(x*z-y*w)*pnt.x + 2.0*(x*w+y*z)*pnt.y + (w2-x2-y2+z2)*pnt.z
         
-        return Vector3(dx, dy, dz)
+        return Vector3.Vector3(dx, dy, dz)
         
     def toMatrix4(self):
         '''Converts a rotation to 4x4 matrix.'''
@@ -95,7 +98,7 @@ class Quaternion(object):
         w = self.w
         
         import Matrix4
-        matrix = Matrix4()
+        matrix = Matrix4.Matrix4()
         matrix.m11 = 1.0-2.0*(y*y+z*z)
         matrix.m12 = 2.0*(x*y-z*w)
         matrix.m13 = 2.0*(x*z+y*w)
@@ -131,58 +134,26 @@ class Quaternion(object):
             t = math.acos(self.w)
             scale = math.sin(t)
             if Util.isEqualZero(scale):
-                return Vector3(0,0,1), 0.0
+                return Vector3.Vector3(0,0,1), 0.0
             else:
-                axis = Vector3(self.x / scale, self.y / scale, self.z / scale)
+                axis = Vector3.Vector3(self.x / scale, self.y / scale, self.z / scale)
                 return axis, 2*t
         else:
-            return Vector3(0,0,1), 0.0
-            
-    def toEulerXYZ(self):
-        def clamp(x):
-            return min( max( x, -1 ), 1 )
-            
-        sqx = self.x * self.x
-        sqy = self.y * self.y
-        sqz = self.z * self.z
-        sqw = self.w * self.w
-        
-        import Vector3
-        euler = Vector3.Vector3()
-        euler.x = math.atan2( 2 * ( self.x * self.w - self.y * self.z ), ( sqw - sqx - sqy + sqz ) )
-        euler.y = math.asin(  clamp( 2 * ( self.x * self.z + self.y * self.w ) ) )
-        euler.z = math.atan2( 2 * ( self.z * self.w - self.x * self.y ), ( sqw + sqx - sqy - sqz ) )
-        return euler
-        
-    def toEulerZXY(self):
-        def clamp(x):
-            return min( max( x, -1 ), 1 )
-            
-        sqx = self.x * self.x
-        sqy = self.y * self.y
-        sqz = self.z * self.z
-        sqw = self.w * self.w
-        
-        import Vector3
-        euler = Vector3.Vector3()
-        euler.x = math.asin(  clamp( 2 * ( self.x * self.w + self.y * self.z ) ) )
-        euler.y = math.atan2( 2 * ( self.y * self.w - self.z * self.x ), ( sqw - sqx - sqy + sqz ) )
-        euler.z = math.atan2( 2 * ( self.z * self.w - self.x * self.y ), ( sqw - sqx + sqy - sqz ) )
-        return euler
+            return Vector3.Vector3(0,0,1), 0.0
         
     def setIdentity(self):
         self.set(0.0, 0.0, 0.0, 1.0)
         return self
         
     @staticmethod
-    def Identity():
+    def identity():
         '''Returns the identity quaternion.'''
         return Quaternion(0.0, 0.0, 0.0, 1.0)
         
     @staticmethod
-    def Matrix4(matrix):
+    def matrix4(matrix):
         import Matrix4
-        assert isinstance(matrix, Matrix4)
+        assert isinstance(matrix, Matrix4.Matrix4)
 
         quat = Quaternion()
         M = matrix
@@ -216,22 +187,18 @@ class Quaternion(object):
         return quat
         
     @staticmethod
-    def AxisAngle(axis, angle):
+    def axisAngle(axis, angle):
         '''Creates a rotation which rotates angle degrees around axis.'''
-        import Vector3
-        assert isinstance(axis, Vector3) and \
-               type(angle) in (int, long, float)
-        
-        return Quaternion.AxisAngleInRadian(axis, Util.degreeToRadian(angle))
+        return Quaternion.axisAngleInRadian(axis, Util.degreeToRadian(angle))
         
     @staticmethod
-    def AxisAngleInRadian(axis, angle):
+    def axisAngleInRadian(axis, angle):
         '''Creates a rotation which rotates angle degrees around axis.'''
         import Vector3
-        assert isinstance(axis, Vector3) and \
+        assert isinstance(axis, Vector3.Vector3) and \
                type(angle) in (int, long, float)
         
-        axis = axis.normalized()
+        axis = axis.normalized
         scale = math.sin(angle / 2)
         
         quat = Quaternion()
@@ -243,67 +210,27 @@ class Quaternion(object):
         return quat
         
     @staticmethod
-    def FromToRotation(f, to):
+    def fromToRotation(f, to):
         '''Creates a rotation which rotates from from(Vector) to to(Vector).'''
-        import Vector3
+        from Vector3 import Vector3
         assert isinstance(f, Vector3) and isinstance(to, Vector3)
         
         # reference:FreeCAD Rotation.cpp
-        u = f.normalized()
-        v = to.normalized()
-        dot = u.dot(v)
-        w = u.cross(v)
+        u = f.normalized
+        v = to.normalized
+        dot = Vector3.dot(u, v)
+        w = Vector3.cross(u, v)
         
         # parallel vectors
-        if w.magnitude() == 0:
+        if w.length == 0:
             # same direction
             if dot >= 0:
                 return Quaternion(0.0, 0.0, 0.0, 1.0)
             else:
-                t = u.cross(Vector3(1.0, 0.0, 0.0))
-                if Util.isEqualZero(t.magnitude()):
-                    t = u.cross(Vector3(0.0, 1.0, 0.0))
+                t = Vector3.cross(u, Vector3(1.0, 0.0, 0.0))
+                if Util.isEqualZero(t.length):
+                    t = Vector3.cross(u, Vector3(0.0, 1.0, 0.0))
                 return Quaternion(t.x, t.y, t.z, 0.0)
         else:
             angleInRad = math.acos(dot)
-            return Quaternion.AxisAngleInRadian(w, angleInRad)
-            
-    @staticmethod
-    def EulerXYZ(x, y, z):
-        x = Util.degreeToRadian(x)
-        y = Util.degreeToRadian(y)
-        z = Util.degreeToRadian(z)
-        
-        c1 = math.cos(x / 2)
-        c2 = math.cos(y / 2)
-        c3 = math.cos(z / 2)
-        s1 = math.sin(x / 2)
-        s2 = math.sin(y / 2)
-        s3 = math.sin(z / 2)
-        
-        quat = Quaternion()
-        quat.x = s1 * c2 * c3 + c1 * s2 * s3
-        quat.y = c1 * s2 * c3 - s1 * c2 * s3
-        quat.z = c1 * c2 * s3 + s1 * s2 * c3
-        quat.w = c1 * c2 * c3 - s1 * s2 * s3
-        return quat
-        
-    @staticmethod
-    def EulerZXY(x, y, z):
-        x = Util.degreeToRadian(x)
-        y = Util.degreeToRadian(y)
-        z = Util.degreeToRadian(z)
-        
-        c1 = math.cos(x / 2)
-        c2 = math.cos(y / 2)
-        c3 = math.cos(z / 2)
-        s1 = math.sin(x / 2)
-        s2 = math.sin(y / 2)
-        s3 = math.sin(z / 2)
-        
-        quat = Quaternion()
-        quat.x = s1 * c2 * c3 - c1 * s2 * s3
-        quat.y = c1 * s2 * c3 + s1 * c2 * s3
-        quat.z = c1 * c2 * s3 + s1 * s2 * c3
-        quat.w = c1 * c2 * c3 - s1 * s2 * s3
-        return quat
+            return Quaternion.axisAngleInRadian(w, angleInRad)
